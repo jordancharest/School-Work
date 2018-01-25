575e52a858002ac49b2e0cbc1a2c059c7f0ddeab
Jordan <jordancharest3@gmail.com>
2018-01-25 13:46:53 -0500

    Added functionality to detect regular files

--- Operating Systems/Homework/Directory-Navigation/directory_navigation.c
+++ Operating Systems/Homework/Directory-Navigation/directory_navigation.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+
 #include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <dirent.h>
 
 #define WORD_SIZE 80
@@ -20,10 +22,34 @@ unsigned int word_count = 0;
 unsigned int space_avail = INITIAL_SIZE;
 
 // READ DIRECTORY CONTENTS ===============================================================
-void read_directory_contents(DIR* directory, alnum_t* dictionary) {
+void read_directory_contents(DIR* directory, alnum_t* dictionary, char* filepath) {
     struct dirent* dir_pt;
-    while ((dir_pt = readdir(directory)) != NULL){
+    struct stat filedata;
+    char full_path[NAME_MAX];
 
+    while ((dir_pt = readdir(directory)) != NULL){
+        printf("The current file is: %s\n", dir_pt->d_name);
+        sprintf(full_path, "%s/%s", filepath, dir_pt->d_name);
+
+
+        int st_err = lstat(full_path, &filedata);     // get file data for current file pointed to by dir_pt
+        if (st_err == -1) {
+                perror("Failed to read file data");
+                exit(EXIT_FAILURE);
+        }
+
+        // check if the current file is a regular file
+        if (S_ISREG(filedata.st_mode)){
+            printf("Is regular file\n");
+            printf("File size: %ld\n", filedata.st_size);
+
+            //read_file_contents(dictionary);
+
+
+        } else {
+            printf("  Not a regular file\n\n");
+            continue;
+        }
     }
 
     closedir(directory);
@@ -45,7 +71,7 @@ int main(int argc, char** argv){
     alnum_t* dictionary = malloc(INITIAL_SIZE * (sizeof(unsigned int) + WORD_SIZE));
     if ( dictionary == NULL )
     {
-        fprintf( stderr, "malloc() failed" );
+        fprintf( stderr, "malloc() failed\n" );
         return EXIT_FAILURE;
     }
 
@@ -53,14 +79,15 @@ int main(int argc, char** argv){
 
     // Attempt to open the specified directory
     char* filepath = argv[1];
+    printf("Current Working Directory: %s\n\n", filepath);
     DIR* directory = opendir(filepath);
 
     if (directory == NULL){
-        perror("Cannot open");
+        perror("Cannot open directory");
         return EXIT_FAILURE;
     }
 
-    read_directory_contents(directory, dictionary);
+    read_directory_contents(directory, dictionary, filepath);
 
     free(dictionary);
     dictionary = NULL;
