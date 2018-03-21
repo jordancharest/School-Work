ac51940e7eadd5fce7923e284aceb6f29dcabeb6
jordancharest <31459889+jordancharest@users.noreply.github.com>
2018-03-20 23:11:15 -0400

    Fixed formatting

--- High Performance Computing/Homework/MPI_P2P_Reduce/MPI_P2P_reduce.c
+++ High Performance Computing/Homework/MPI_P2P_Reduce/MPI_P2P_reduce.c
@@ -33,20 +33,19 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	// ensure the world size is a power of 2
-	int i = 1;
-	for (; i < 20; i++) {
-		if (world_size == pow(2,i))
-			break;
-	}
-
-    if (i == 20) {
-		if (rank == 0)
-			fprintf(stderr, "ERROR: use a multiple of two processes\n");
-		exit(EXIT_FAILURE);
+    // ensure the world size is a power of 2
+    int i = 1;
+    for (; i < 20; i++) {
+        if (world_size == pow(2,i))
+            break;
     }
 
-    long long* array = NULL;
+    if (i == 20) {
+        if (rank == 0)
+            fprintf(stderr, "ERROR: use a multiple of two processes\n");
+        exit(EXIT_FAILURE);
+    }
+
     int elements_per_proc = ARRAY_SIZE/world_size;
 
     long long* sub_array = malloc(sizeof(long long) * elements_per_proc);
@@ -60,30 +59,29 @@ int main(int argc, char** argv) {
     }
 
     // My implementation of MPI_Reduce (MPI_P2P_Reduce)
-	start_cycles = GetTimeBase();
+    start_cycles = GetTimeBase();
     long long homemade_sum = MPI_P2P_reduce(sub_array, elements_per_proc, world_size, rank);
-	end_cycles = GetTimeBase();
-	time_in_secs = ((double)(end_cycles - start_cycles))/processor_frequency;
+    end_cycles = GetTimeBase();
+    time_in_secs = ((double)(end_cycles - start_cycles))/processor_frequency;
 
-	if (rank == 0)
-		printf("MPI_P2P_Reduce time: %f\n", time_in_secs);
+    if (rank == 0)
+        printf("MPI_P2P_Reduce time: %f\n", time_in_secs);
 
 
-    // My implementation of MPI_Reduce (MPI_P2P_Reduce)
-	start_cycles = GetTimeBase();
+    // Library implementation of MPI_Reduce
+    start_cycles = GetTimeBase();
     long long process_sum = sum_per_process(sub_array, elements_per_proc);
     long long mpi_sum;
     MPI_Reduce(&process_sum, &mpi_sum, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-	end_cycles = GetTimeBase();
-	time_in_secs = ((double)(end_cycles - start_cycles))/processor_frequency;
+    end_cycles = GetTimeBase();
+    time_in_secs = ((double)(end_cycles - start_cycles))/processor_frequency;
 
-	if (rank == 0)
-		printf("MPI_Reduce time: %f\n", time_in_secs);
+    if (rank == 0)
+        printf("MPI_Reduce time: %f\n", time_in_secs);
 
     if (rank == 0) {
-		if ((ACTUAL_SUM != mpi_sum)  ||  (ACTUAL_SUM != homemade_sum)) {
-	    	printf("Sum failed...\n");
-		}
+        if ((ACTUAL_SUM != mpi_sum)  ||  (ACTUAL_SUM != homemade_sum))
+            printf("Sum failed...\n");
 
     	printf(" Actual Sum: %lld\n MPI Sum: %lld\n Homemade Sum: %lld\n", ACTUAL_SUM, mpi_sum, homemade_sum);
     }
@@ -119,85 +117,85 @@ long long MPI_P2P_reduce(long long* array, int array_size, int world_size, int r
 
 	// Dynamic receives, different ranks receive a different number of times
     for (int i = 0;  i < total_reductions;  i++, receivers*=2) {
-		if (rank == 0  ||  (rank%receivers == 0 && rank >= receivers)) {
-	    	num_receives++;
+        if (rank == 0  ||  (rank%receivers == 0 && rank >= receivers)) {
+            num_receives++;
 
-	    	if (i == 0)
-				source = rank+1;
-	    	else
-				source = rank + ((int)pow(2,i));
+	    if (i == 0)
+		source = rank+1;
+	    else
+		source = rank + ((int)pow(2,i));
 
 
-    		MPI_Irecv((received_sum + i), 1, MPI_LONG_LONG, source, 0, MPI_COMM_WORLD, (receive_requests + i));
+    	    MPI_Irecv((received_sum + i), 1, MPI_LONG_LONG, source, 0, MPI_COMM_WORLD, (receive_requests + i));
 
-			#ifdef DEBUG_MODE
-				if (rank == 0 || rank == 16)
-    	    		printf("Rank %d: receiving from %d\n", rank, source);
-			#endif
+	    #ifdef DEBUG_MODE
+		if (rank == 0 || rank == 16)
+    		printf("Rank %d: receiving from %d\n", rank, source);
+	    #endif
 
-		} else
-	    	break;
+	} else
+	    break;
     }
 
 
     long long process_sum = sum_per_process(array, array_size);
 
-	// every process except for zero posts one send
-	int destination = 0;
-	int dest_found = 0;
-	int modifier = 2;
-	int num_sends = 0;
-	int i = 0;
-	MPI_Request send_request;
+    // every process except for zero posts one send
+    int destination = 0;
+    int dest_found = 0;
+    int modifier = 2;
+    int num_sends = 0;
+    int i = 0;
+    MPI_Request send_request;
 
-	if (rank > 0) {
-		while (!dest_found) {
-			if (rank % modifier != 0) {
-				if (i == 0) destination = rank-1;
-				else destination = rank - (int)pow(2,i);
+    if (rank > 0) {
+        while (!dest_found) {
+            if (rank % modifier != 0) {
+                if (i == 0) destination = rank-1;
+		else destination = rank - (int)pow(2,i);
 
-				dest_found = 1;
-				num_sends++;
+		dest_found = 1;
+		num_sends++;
 
-			} else if (rank == modifier) {
-				destination = 0;
-				dest_found = 1;
-				num_sends++;
-			}
+            } else if (rank == modifier) {
+                destination = 0;
+                dest_found = 1;
+                num_sends++;
+            }
 
-			i++;
-			modifier *= 2;
-		}
-	}
-
-
-	#ifdef DEBUG_MODE
-		printf("Rank %d: waiting for %d messages\n", rank, num_receives);
-	#endif
-
-	for (int j = 0; j < num_receives; j++) {
-		MPI_Wait((receive_requests + j), MPI_STATUS_IGNORE);
-		process_sum += received_sum[j];
-
-		#ifdef DEBUG_MODE
-			printf("Rank %d: received %lld in message %d;  new sum is %lld\n", rank, received_sum[j], j+1, process_sum);
-		#endif
-	}
+            i++;
+            modifier *= 2;
+        }
+    }
 
 
-	if (rank > 0) {
-		#ifdef DEBUG_MODE
-			printf("Rank %d: sending %lld to rank %d\n", rank, process_sum, destination);
-		#endif
+    #ifdef DEBUG_MODE
+        printf("Rank %d: waiting for %d messages\n", rank, num_receives);
+    #endif
 
-		MPI_Isend(&process_sum, 1, MPI_LONG_LONG, destination, 0, MPI_COMM_WORLD,  &send_request);
-	}
+    for (int j = 0; j < num_receives; j++) {
+        MPI_Wait((receive_requests + j), MPI_STATUS_IGNORE);
+        process_sum += received_sum[j];
+
+        #ifdef DEBUG_MODE
+            printf("Rank %d: received %lld in message %d;  new sum is %lld\n", rank, received_sum[j], j+1, process_sum);
+        #endif
+    }
 
 
-	#ifdef DEBUG_MODE
-		printf("Rank %d: Exiting\n", rank);
+    if (rank > 0) {
+        #ifdef DEBUG_MODE
+            printf("Rank %d: sending %lld to rank %d\n", rank, process_sum, destination);
+        #endif
+
+        MPI_Isend(&process_sum, 1, MPI_LONG_LONG, destination, 0, MPI_COMM_WORLD,  &send_request);
+    }
+
+
+    #ifdef DEBUG_MODE
+        printf("Rank %d: Exiting\n", rank);
     #endif
 
 
-	return process_sum;
+    return process_sum;
 }
