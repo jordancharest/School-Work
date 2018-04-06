9080915f041a7fa7159057b244703c0bb23840fd
Jordan <jordancharest3@gmail.com>
2018-04-06 18:49:13 -0400

    Increased max allowable world size for Blue Gene/Q

--- High Performance Computing/Homework/Game_of_Life/game_of_life.c
+++ High Performance Computing/Homework/Game_of_Life/game_of_life.c
@@ -20,7 +20,7 @@
 const unsigned int BOARD_SIZE = 16384;
 const unsigned int TICKS = 128;
 const double THRESHOLD = 0.25;
-const int MAX_WORLD_SIZE = 128;
+const int MAX_WORLD_SIZE = 256;
 
 unsigned int rows_per_thread = 0;
 unsigned int rows_per_rank = 0;
