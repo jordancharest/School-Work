8b7dc62858fb84a18878dbbf1758bfc1a1c0b721
Jordan <jordancharest3@gmail.com>
2018-04-22 17:21:35 -0400

    Started A* serial implementation and created header file

---
+++ High Performance Computing/Project/serial.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "maze.h"
+
+extern struct Node* nodes;  // maze; contained in one dimensional array
+
+// SERIAL ========================================================================================
+void serial_A_star(int width, int height) {
+
+    int x = 1;
+    int y = 1;
+    int x_goal = width - 2;
+    int y_goal = height - 2;
+
+    nodes[x + y * width].c = 'S';
+    nodes[x_goal + y_goal * width].c = 'G';
+    draw();
+
+
+
+
+
+
+}
+
