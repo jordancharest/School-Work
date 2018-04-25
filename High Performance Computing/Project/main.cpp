b21bfc7de125732c772eefd579599a6995e9475d
Jordan <jordancharest3@gmail.com>
2018-04-25 01:06:19 -0400

    Added ability to set noise

--- High Performance Computing/Project/main.cpp
+++ High Performance Computing/Project/main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <thread>
 
 #include "robot.hpp"
 
+// random generator for particle initialization
 std::default_random_engine generator;
 std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
+
+// GLOBAL
+int N = 10;    // number of particles
+
+// PARTICLE FILTER ===============================================================================
+void particle_filter(Robot &robot, std::vector<Robot> &particles, double sensor_noise) {
+
+    int t = 0;
+    double allowable = 0.33 * sensor_noise;
+    double mean_error = 100.0;
+
+    // simulate the robot moving about its environment until the solution converges
+    while (mean_error > allowable) {
+
+
+
+
+    }
+
+
+
+
+
+}
 
 // MAIN ==========================================================================================
 int main(int argc, char** argv) {
@@ -18,17 +44,28 @@ int main(int argc, char** argv) {
     generator.seed(seed);
 
 
-
-    int N = 10;    // number of particles
     int world_size = 10;   // circular world
+    double sensor_noise = 3.0;
     std::vector<Point> landmarks;
+
+    Robot robot(world_size, landmarks);
+    robot.setNoise(0.0, 0.0, sensor_noise);
 
     // Initialize N random particles
     std::vector<Robot> particles;
     for (int i = 0; i < N; i++) {
+        Robot particle(world_size, landmarks);
+        particle.setNoise(0.0, 0.0, sensor_noise);
         particles.push_back(Robot(world_size, landmarks));
-        //std::cout << "Particle initialized to: (" << particles[i].x() << ", " << particles[i].y() << ")\n";
     }
+
+    #ifdef DEBUG
+        for (auto &particle : particles) {
+            std::cout << "Particle initialized to: (" << particle.x() << ", " << particle.y() << ")\n";
+        }
+    #endif
+
+    particle_filter(robot, particles, sensor_noise);
 
 
 
