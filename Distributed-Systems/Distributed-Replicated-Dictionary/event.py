3cf042c2f17ddba5c64e2512ff2f4bdc65e28988
Jordan Charest <jordancharest3@gmail.com>
2018-09-29 13:51:40 -0400

    Better string representation of event class

--- Distributed-Systems/Distributed-Replicated-Dictionary/event.py
+++ Distributed-Systems/Distributed-Replicated-Dictionary/event.py
@@ -1,18 +1,17 @@
 class event:
-    def __init__(self, name, day, start, end, participants):
-        self.day = day
+    def __init__(self, name, date, start, end, participants):
+        self.date = date
         self.start = start
         self.end = end
         self.name = name
         self.participants = participants
 
     def __lt__(self, other):
-        return ((self.day < other.day) or
-               (self.day == other.day and self.start < other.start) or
-               (self.day == other.day and self.start == other.start and self.name < other.name))
+        return ((self.date < other.date) or
+               (self.date == other.date and self.start < other.start) or
+               (self.date == other.date and self.start == other.start and self.name < other.name))
 
     def __repr__(self):
-        result = self.name + " " + self.day + " " + self.start + " " + self.end
-        for p in self.participants:
-            result += " " + p + ","
-        return result[:-1]
+        result = self.name + " " + self.date + " " + self.start + " " + self.end + " "
+        result += ",".join(self.participants)
+        return result
