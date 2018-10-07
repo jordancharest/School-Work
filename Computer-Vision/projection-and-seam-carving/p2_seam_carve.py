1f837ad48466af040a9ceb694dfebb9e9c3ef81d
Jordan Charest <jordancharest3@gmail.com>
2018-10-07 16:37:12 -0400

    Energy and cost calculations completed; seam endpoint found

--- Computer-Vision/projection-and-seam-carving/p2_seam_carve.py
+++ Computer-Vision/projection-and-seam-carving/p2_seam_carve.py
@@ -16,10 +16,72 @@ def arg_parse():
 
     return img
 
+# -----------------------------------------------------------------------------
+def calculate_energy(img):
+    # compute energy (sum of gradients) from grayscaled image
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3, borderType=cv2.BORDER_DEFAULT)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3, borderType=cv2.BORDER_DEFAULT)
+    energy = np.abs(sobelx) + np.abs(sobely)
+
+    # assign very high values to the first and last columns 
+    # to prevent the seam from reaching them
+    energy[:,0] = 1e7
+    energy[:,-1] = 1e7
+
+    return energy
 
 
+# -----------------------------------------------------------------------------
+def calculate_cost(energy):
+    # first row of cost function equals energy of that row 
+    cost = np.zeros(energy.shape)
+    cost[0,:] = energy[0,:]
+
+    # calculate the cost of each pixel, row by row
+    for i in range(energy.shape[0]-1):
+        # already calculated the first row
+        i += 1
+
+        # compute the costs on the edges (artificially high)
+        cost[i, 0] = energy[i, 0] + cost[i-1, 0]
+        cost[i, -1] = energy[i, 1] + cost[i-1, -1]
+
+        # compute the costs in the middle
+        # minimum of           right           left           middle
+        prev = np.minimum(cost[i-1, :-2], cost[i-1, 2:], cost[i-1, 1:-1])
+        cost[i,1:-1] = energy[i,1:-1] + prev
+
+    return cost
 
 
+# =============================================================================
 if __name__ == "__main__":
     img = arg_parse()
     print(img.shape)
+
+    # carve seams until the image is square
+    while img.shape[0] != img.shape[1]:
+        energy = calculate_energy(img)
+        cost = calculate_cost(energy)
+        # seam = find_seam(cost)
+        # remove_seam(img)
+
+
+
+        # backtrack to find the seam - endpoint is the minimum energy at the bottom of the image
+        endpoint = np.argmin(cost[-1, :])
+        for i in range(energy.shape[0]):
+
+            # endpoint is the minimum energy at the bottom of the image
+            img[-1, endpoint] = [0,0,255]
+            break
+
+
+        print(np.max(energy))
+        print(energy)
+        print(cost)
+        break
+
+
+    cv2.imwrite("endpoint.jpg", img)
