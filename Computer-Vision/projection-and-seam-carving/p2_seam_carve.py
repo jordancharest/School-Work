def09ad0092ddaa7d1a7278b3996c649a7b7d51e
Jordan Charest <jordancharest3@gmail.com>
2018-10-07 14:04:10 -0400

    Read image

--- Computer-Vision/projection-and-seam-carving/p2_seam_carve.py
+++ Computer-Vision/projection-and-seam-carving/p2_seam_carve.py
@@ -1,14 +1,20 @@
 from sys import argv
 
+import numpy as np
+import cv2
+
 
 # -----------------------------------------------------------------------------
 def arg_parse():
     if len(argv) == 2:
-        pass
+        script, img_name = argv
+        img = cv2.imread(img_name, cv2.IMREAD_COLOR)
     else:
         print("Invalid Argument(s).")
         print("USAGE: {0} <img>".format(argv[0]))
         exit()
+
+    return img
 
 
 
@@ -16,3 +22,4 @@ def arg_parse():
 
 if __name__ == "__main__":
     img = arg_parse()
+    print(img.shape)
