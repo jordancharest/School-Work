8d5b079f72ec6b18b82693c18085031aab307b07
Jordan Charest <jordancharest3@gmail.com>
2018-10-10 14:17:42 -0400

    Added output statistics

--- Computer-Vision/projection-and-seam-carving/p2_seam_carve.py
+++ Computer-Vision/projection-and-seam-carving/p2_seam_carve.py
@@ -1,5 +1,6 @@
 from sys import argv
 import ntpath
+import math as m
 
 import numpy as np
 import cv2
@@ -28,8 +29,13 @@ def arg_parse():
 def calculate_energy(img):
     img = np.uint8(img)
 
+    sigma = 1
+    h = m.floor(2.5*sigma)
+    ksize = (3, 3)
+
     # compute energy (sum of gradients) from grayscaled image
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # gray = cv2.GaussianBlur(gray, ksize, sigma, borderType=cv2.BORDER_DEFAULT)
     sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3, borderType=cv2.BORDER_DEFAULT)
     sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3, borderType=cv2.BORDER_DEFAULT)
     energy = np.abs(sobelx) + np.abs(sobely)
@@ -55,7 +61,7 @@ def calculate_cost(energy):
 
         # compute the costs on the edges
         cost[i, 0] = energy[i, 0] + np.minimum(cost[i-1, 0], cost[i-1, 1])
-        cost[i, -1] = energy[i, 1] + np.minimum(cost[i-1, -1], cost[i-1, -2])
+        cost[i, -1] = energy[i, -1] + np.minimum(cost[i-1, -1], cost[i-1, -2])
 
         # compute the costs in the middle
         # minimum of           right           left           middle
@@ -65,15 +71,12 @@ def calculate_cost(energy):
     return cost
 
 # -----------------------------------------------------------------------------
-def find_seam(img, cost, first=False):
+def find_seam(img, cost, filename="", ext="", first=False):
     seam = []
 
     # endpoint is the minimum energy at the bottom of the image
     index = np.argmin(cost[-1, :])
     seam.append(index)
-
-    # print("Endpoint:", index)
-    # print(cost[-1, index])
 
     # paint the seam on the image if it's the first run through
     if first:
@@ -92,18 +95,40 @@ def find_seam(img, cost, first=False):
 
 
 
+    if first:
+        cv2.imwrite(filename + "_seam." + ext, img)
+
     # seam is now a list of indices from bottom of image to top
-    return seam
+    # return it from top to bottom order
+    return seam[::-1], cost[-1, index] / img.shape[0]
    
 # -----------------------------------------------------------------------------
 def remove_seam(img, seam):
     result = np.zeros((img.shape[0], img.shape[1]-1, img.shape[2]))
 
     # copy every pixel in each row except for the seam pixel
-    for j,index in enumerate(seam[::-1]):
+    for j,index in enumerate(seam):
         result[j, :, :] = img[j, np.r_[0:index, index+1:result.shape[1]+1], :]
 
     return result
+
+
+# -----------------------------------------------------------------------------
+def output(i, img, seam, rotated, avg_energy):
+    if i == 0:
+        for j, index in enumerate(seam):
+            print("Writing on image at", j, index)
+    
+
+    print("\nPoints on seam {0}:".format(i))
+    if rotated:
+        print("horizontal")
+    else:
+        print("vertical")
+    print("{0}, {1}".format(0, seam[0]))
+    print("{0}, {1}".format(img.shape[0] // 2, seam[img.shape[0] // 2]))
+    print("{0}, {1}".format(img.shape[0]-1, seam[img.shape[0]-1]))
+    print("Energy of seam {0}: {1:.2f}".format(i, avg_energy))
 
 
 # =============================================================================
@@ -117,14 +142,31 @@ if __name__ == "__main__":
         img = np.rot90(img, k=1, axes=(1,0))
         rotated = True
 
+
+    
     # carve seams until the image is square
+    i = 0
     while img.shape[0] != img.shape[1]:
         # print("Finding seam:", img.shape)
         energy = calculate_energy(img)
         cost = calculate_cost(energy)
-        seam = find_seam(img, cost, True)
-        img = remove_seam(img, seam)
 
+        # draw the seam on the first run through
+        if i == 0:
+            seam, avg_energy = find_seam(img, cost, filename, ext, True)
+        else:
+            seam, avg_energy = find_seam(img, cost)
+
+        # print some statistics for the first and second runs
+        if i == 0 or i == 1:
+            output(i, img, seam, rotated, avg_energy)
+
+        img = remove_seam(img, seam)
+        i += 1
+
+    i -= 1
+    # print some statistics for the last run
+    output(i, img, seam, rotated, avg_energy)
 
     print(img.shape)
     if rotated:
