bb23b9059f13e2e449c79ecae272760051f25756
Jordan Charest <jordancharest3@gmail.com>
2018-10-17 21:13:01 -0400

    Gradient magnitude calculation

--- Computer-Vision/keypoints-and-edge-detection/p1_edge.py
+++ Computer-Vision/keypoints-and-edge-detection/p1_edge.py
@@ -1,28 +1,69 @@
 from sys import argv
+import math as m
 import ntpath
 
 import numpy as np
 import cv2
+
+# -----------------------------------------------------------------------------
+def plot_pics(image_list, num_cols=2, title_list=[]):
+    if len(image_list) == 0: return
+    if len(image_list[0].shape) == 2:
+        plt.gray()
+    num_rows = int( m.ceil(len(image_list)/float(num_cols)))
+
+    if num_cols > 2:
+        plt.figure(figsize=(10,10))
+    else:
+        plt.figure(figsize=(15,15))
+
+    for i in range(len(image_list)):
+        im = image_list[i]
+        print(num_rows, num_cols, i+1)
+        plt.subplot(num_rows, num_cols, i+1)
+        plt.imshow(im)
+        if i < len(title_list):
+            plt.title(title_list[i])
+        plt.xticks([]), plt.yticks([])
+
+    plt.show()
+
+
 # -----------------------------------------------------------------------------
 def arg_parse():
     if len(argv) == 3:
-        script, sigma, img_name = argv
-        img = cv2.imread(img_name, cv2.IMREAD_COLOR)
+        script, sigma, path_to_img = argv
 
-        # extract image name
-        _, filename = ntpath.split(path)
-        split = filename.split(".")
-        filename = split[0]
-        ext = split[1]
+        # extract image name and read it
+        _, filename = ntpath.split(path_to_img)
+        filename, ext = filename.split(".")
+        img = cv2.imread(path_to_img, cv2.IMREAD_COLOR)
 
-        return sigma, img, img_name, ext
+        return float(sigma), img, filename, ext
 
     else:
         print("Invalid Argument(s).")
         print("USAGE: {} <sigma> <image>".format(argv[0]))
         exit()
 
+# -----------------------------------------------------------------------------
+def calculate_gradients(img, sigma=1.0):
+    ksize = (int(4*sigma+1), int(4*sigma+1))
+    blurred = cv2.GaussianBlur(img, ksize, sigma)
+
+    kx,ky = cv2.getDerivKernels(1,1,3)
+    kx = np.transpose(kx/2)
+    ky = ky/2
+    x_gradient = cv2.filter2D(blurred, -1, kx)
+    y_gradient = cv2.filter2D(blurred, -1, ky)
+    gradient_magnitude = np.sqrt(x_gradient**2 + y_gradient**2)
+    return gradient_magnitude
+
 
 # =============================================================================
 if __name__ == "__main__":
     sigma, img, img_name, ext = arg_parse()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).astype(float)
+    print(gray.dtype)
+
+    gradient = calculate_gradients(gray, sigma)
