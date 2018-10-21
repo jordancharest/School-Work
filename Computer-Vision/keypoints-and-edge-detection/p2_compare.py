c2a5a23be18a78a0e548bb6e0f1cea17f4ae361d
Jordan Charest <jordancharest3@gmail.com>
2018-10-21 13:00:42 -0400

    Completed keypoint match and output statistics

--- Computer-Vision/keypoints-and-edge-detection/p2_compare.py
+++ Computer-Vision/keypoints-and-edge-detection/p2_compare.py
@@ -107,12 +107,13 @@ def extract_harris_keypoints(original, img_harris, sigma, apply_threshold=False)
     # form structuring kernel and dilate
     max_dist = int(2*sigma)
     kernel = np.ones((2*max_dist+1, 2*max_dist+1), np.uint8)
-    img_harris = img_harris.astype(np.uint8)
+    # img_harris = img_harris.astype(np.uint8)
     dilated = cv2.dilate(img_harris, kernel)
 
     # Comparing the dilated image to the Harris image will preserve only those
     # locations that are peaks (non-maximum suppression)
-    harris_peaks = img_harris >= dilated
+    # harris_peaks = img_harris >= dilated
+    harris_peaks = cv2.compare(img_harris, dilated, cv2.CMP_GE)
 
     # Multiplying by the thresholded image keeps only peaks above the threshold
     # or multiply by the original to keep all peaks
@@ -121,48 +122,101 @@ def extract_harris_keypoints(original, img_harris, sigma, apply_threshold=False)
     else:
         harris_peaks = harris_peaks * img_harris
 
+    # grayscale normalization
+    harris_peaks = 255 * (harris_peaks - np.min(harris_peaks)) / \
+                    (np.max(harris_peaks) - np.min(harris_peaks))
+
     # Extract all indices of the peaks and build keypoint list
     ys, xs = np.where(harris_peaks > 0)
-    kp_size = 4*sigma
+
+    # TODO ask question about harris keypoint size
+    kp_size = 2*sigma
     harris_keypoints = [
-        cv2.KeyPoint(xs[i], ys[i], kp_size)
+        cv2.KeyPoint(xs[i], ys[i], kp_size, _response=harris_peaks[ys[i]][xs[i]])
         for i in range(len(xs))
     ]
 
-    print("We have", len(harris_keypoints), "keypoints.")
+    # print("We have", len(harris_keypoints), "keypoints.")
 
     out_img = cv2.drawKeypoints(original.astype(np.uint8), harris_keypoints, None)
     cv2.imwrite("Keypoints.jpg", out_img)
-    # plot_pics( [img_harris, out_img], 1, ['Harris Measure', 'Keypoints shown'] )
+
+    # sort by keypoint response, largest first
+    harris_keypoints.sort( key = lambda k: k.response)
+    harris_keypoints = harris_keypoints[::-1]
+
+    print("\nTop 10 Harris keypoints:")
+    for i,k in enumerate(harris_keypoints[:10]):
+        print("{0}: ({1:.2f}, {2:.2f}) {3:.4f} {4:.2f}".format(i, k.pt[0], 
+                                                        k.pt[1], k.response, 
+                                                        k.size))
 
     return harris_keypoints
 
 # -----------------------------------------------------------------------------
-def extract_SIFT_keypoints(img,):
+def extract_SIFT_keypoints(img, sigma):
     sift = cv2.xfeatures2d.SIFT_create()
     kp = sift.detect(img, None)
 
-    print(type(kp))
-    print(len(kp))
     kp.sort( key = lambda k: k.response)
     kp = kp[::-1]
     out_img = cv2.drawKeypoints(img.astype(np.uint8), kp, None)
 
     '''
-    The SIFT result includes many keypoints at the same location, but with different orientations.
-    This filters out the keypoints down to just one per location.  In matching keypoints between
-    images, this filtering is not a good idea because each orientation will create a different
-    descriptor.
+    The SIFT result includes many keypoints at the same location, but with 
+    different orientations. This filters out the keypoints down to just one
+    per location. In matching keypoints between images, this filtering is not 
+    a good idea because each orientation will create a different descriptor.
     '''
-    kp_unique = [kp[0]]
+    kp_unique = []
+    print("3*sigma =", 3*sigma)
     for k in kp[1:]:
-        if k.pt != kp_unique[-1].pt:
+        if (not kp_unique or k.pt != kp_unique[-1].pt) and k.size <= 3*sigma:
             kp_unique.append( k )
 
-    print(len(kp_unique))
-    # for k in kp_unique:
-        # print(k.response, k.pt, k.angle, k.size)
+    print("\nTop 10 SIFT keypoints:")
+    for i, k in enumerate(kp_unique[:10]):
+        print("{0}: ({1:.2f}, {2:.2f}) {3:.4f} {4:.2f}".format(i, k.pt[0], 
+                                                        k.pt[1], k.response, 
+                                                        k.size))
+    return kp_unique
 
+
+# -----------------------------------------------------------------------------
+def keypoint_match(set1, set2, set1_name, set2_name):
+    last_set1 = min(len(set1), 100)
+    last_set2 = min(len(set2), 200)
+
+    rank_differences = []
+    dist_differences = []
+
+    # for each set1 keypoint in the top 100, find the closest set2
+    # keypoint in the top 200
+    for i in range(last_set1):
+        min_distance = 1e6
+        min_rank = 1e6
+        for j in range(last_set2):
+            distance = m.sqrt((set1[i].pt[0] - set2[j].pt[0])**2 + \
+                              (set1[i].pt[1] - set2[j].pt[1])**2)
+            if distance < min_distance:
+                min_distance = distance
+                min_rank = abs(i-j)
+
+        dist_differences.append(min_distance)
+        rank_differences.append(min_rank)
+
+
+    med_dist = np.median(dist_differences)
+    avg_dist = np.mean(dist_differences)
+    med_rank = np.median(rank_differences)
+    avg_rank = np.mean(rank_differences)
+
+    print("\n{0} keypoint to {1} distances:".format(set1_name, set2_name))
+    print("num_from", last_set1, "num_to", last_set2)
+    print("Median distance: {0:.2f}".format(med_dist))
+    print("Average distance: {0:.2f}".format(avg_dist))
+    print("Median index difference: {0:.2f}".format(med_rank))
+    print("Average index difference: {0:.2f}".format(avg_rank))
 
 
 # =============================================================================
@@ -171,7 +225,6 @@ if __name__ == "__main__":
 
     img_harris = harris_measure(gray, sigma)
     harris_keypoints = extract_harris_keypoints(gray, img_harris, sigma)
-    extract_SIFT_keypoints(gray)
-
-
-
+    SIFT_keypoints = extract_SIFT_keypoints(gray, sigma)
+    keypoint_match(harris_keypoints, SIFT_keypoints, "Harris", "SIFT")
+    keypoint_match(SIFT_keypoints, harris_keypoints, "SIFT", "Harris")
