af17e55f210683353c0c61165f699fc6d262af32
Jordan Charest <jordancharest3@gmail.com>
2018-10-24 15:59:21 -0400

    Attempting to compute histogram in vectorized manner

--- Computer-Vision/keypoints-and-edge-detection/p3_orientation.py
+++ Computer-Vision/keypoints-and-edge-detection/p3_orientation.py
@@ -1,4 +1,5 @@
 from sys import argv
+import math as m
 
 import numpy as np
 import cv2
@@ -9,7 +10,7 @@ def arg_parse():
     if len(argv) == 4:
         script, sigma, img_name, points_file = argv
         pts = np.loadtxt(points_file, np.int32)
-        img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
+        img = cv2.imread(img_name, cv2.IMREAD_COLOR)
         return float(sigma), img, pts
 
     else:
@@ -30,7 +31,10 @@ def calculate_gradients(img, sigma=1.0):
     y_gradient = cv2.filter2D(blurred, -1, ky)
 
     gradient_magnitude = np.sqrt(x_gradient.astype(np.float64)**2 + y_gradient**2) # np.hypot()
-    gradient_direction = np.arctan2(y_gradient.astype(np.float64), x_gradient)
+    gradient_direction = np.arctan2(y_gradient, x_gradient)
+
+    print("Maximum direction:",np.max(gradient_direction))
+    print("Minimum direction:",np.min(gradient_direction))
 
     # compute the weight function 
     # i.e. smooth the magnitude with sigma_v == 2 * sigma
@@ -41,17 +45,56 @@ def calculate_gradients(img, sigma=1.0):
     return gradient_magnitude, gradient_direction, weight
 
 # -----------------------------------------------------------------------------
-def orientation_voting(weight, magnitude, direction, sigma):
-    pass
-    # separate the weight into the two orientation bins that it is closest
-    # to using linear interpolation
+def orientation_voting(pt, weight, magnitude, direction, sigma):
+    sigma_v = 2*sigma
+    w = round(2*sigma_v)
+    # print("2w + 1 =", 2*w+1)
+
+    # extract the neighborhood around the point
+    row, col = pt
+    w_neighborhood = weight[row-w : row+w+1, col-w : col+w+1]
+    d_neighborhood = direction[row-w : row+w+1, col-w : col+w+1]
+    # print(w_neighborhood.shape)
+    # print(w_neighborhood)
+    # print(d_neighborhood.shape)
+    np.set_printoptions(precision=2)
+    print("Direction Neighborhood:\n",d_neighborhood)
+
+    # convert to degrees
+    deg = 180.0 / m.pi
+    d_neighborhood *= deg
+    print("Votes:\n", d_neighborhood)
+     
+    # determine the distance between inter and 5 for linear interpolation
+    # if votes == 0, then the weight is divided evenly between two bins
+    votes = d_neighborhood % 10
+    rounded = np.round(d_neighborhood, -1)
+    print("\nBefore:\n", votes)
+    votes = np.abs(np.abs(votes) - 5)
+    print("\nAfter:\n", votes)
+    bin_weight  = 1.0 - votes/5.0
+    print("\nAfter2:\n", votes)
+    print("\nRounded:\n", rounded)
+
+
+
+
+
+    # print(bin_size_rad)
 
 
 # =============================================================================
 if __name__ == "__main__":
-    sigma, gray, pts = arg_parse()
+    sigma, img, pts = arg_parse()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).astype(float)
     magnitude, direction, weight = calculate_gradients(gray, sigma)
     print(pts)
 
-    for pt in pts:
-        histogram = orientation_voting(weight, magnitude, direction, sigma)
+    for i, pt in enumerate(pts):
+        histogram = orientation_voting(pt, weight, magnitude, direction, sigma)
+        
+        print("Point {0}: ({1}, {2})".format(i, pt[0], pt[1]))
+        print("Histograms:")
+        # for j in range(-18,18):
+            # print("[{},{}]: ".format(j*10, (j+1)*10))
+        break
