b23369d2552b2e7868d295405deea9311426d0be
Jordan Charest <jordancharest3@gmail.com>
2018-10-29 11:36:56 -0400

    Found keypoint matches for all images

--- Computer-Vision/homography-and-mosaics/hw5_align.py
+++ Computer-Vision/homography-and-mosaics/hw5_align.py
@@ -2,6 +2,7 @@ from sys import argv
 import os
 
 import numpy as np
+from scipy import spatial
 import cv2
 
 
@@ -20,10 +21,71 @@ def arg_parse():
         print("USAGE: {} <image-directory>".format(argv[0]))
         exit()
 
+# -----------------------------------------------------------------------------
+# def extract_keypoints(img_list):
+
+#     keypoint_trees = []
+#     for img_name in img_list:
+
+#         # extract sift_keypoints
+#         img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
+#         sift = cv2.xfeatures2d.SIFT_create()
+#         kp = sift.detect(img, None)
+
+#         # sort?
+#         # kp.sort( key = lambda k: k.response)
+#         # kp = kp[::-1]
+
+#         # create a KD-tree with the keypoints
+#         keypoint_trees.append(spatial.KDTree(kp))
+
+#     return keypoint_trees
+
+
+# # ---------------------------------------------------------------------------
+# def match_keypoints():
+
+
+
+# -----------------------------------------------------------------------------
+def detect_and_match(img_list):
+    # find the keypoints and descriptors of the enchor with SIFT
+    anchor = cv2.imread(img_list[0], cv2.IMREAD_GRAYSCALE)
+    kp_anchor, desc_anchor = sift.detectAndCompute(anchor, None)
+
+    for img_name in img_list[1:]:
+
+        # read SIFT keypoints and descriptors for the new image
+        img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
+        sift = cv2.SIFT()
+        kp2, des2 = sift.detectAndCompute(img, None)
+
+        # 'Fast Library for Approximate Nearest Neighbor search'
+        # Searh for approximate nearest neighbors between the anchor and
+        # the current image
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(des1,des2,k=2)
+
+        # store all the good matches as per Lowe's ratio test
+        good = []
+        for m,n in matches:
+            if m.distance < 0.7*n.distance:
+                good.append(m)
+
+    return good
+
 
 # =============================================================================
 if __name__ == "__main__":
     img_list = arg_parse()
     print(img_list)
 
-    tree = cv2.KDTree()
+    # extract a list containing a kd-tree of keypoints from each image
+    matches = detect_and_match(img_list)
+    # keypoint_trees = extract_keypoints(img_list)
+    # match_keypoints(keypoint_trees)
+
+
