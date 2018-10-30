17aa5f54a9330114037120bb79103326c5fa2edf
Jordan Charest <jordancharest3@gmail.com>
2018-10-29 23:22:12 -0400

    Computed fundamental matrix and separated draw keypoints function

--- Computer-Vision/homography-and-mosaics/hw5_align.py
+++ Computer-Vision/homography-and-mosaics/hw5_align.py
@@ -1,10 +1,15 @@
 from sys import argv
 import os
+import ntpath
 
 import numpy as np
 from scipy import spatial
 import cv2
 
+
+# some tunable params
+MIN_MATCH_COUNT = 25
+FM_RANSAC_THRESHOLD = 0.1
 
 # -----------------------------------------------------------------------------
 def arg_parse():
@@ -22,103 +27,94 @@ def arg_parse():
         exit()
 
 # -----------------------------------------------------------------------------
-# def extract_keypoints(img_list):
+def draw_keypoint_matches(img1, kp1, img2, kp2, matches, out_name,
+                            matchesMask=None):
+    
+    # draw matches on the images together
+    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers (if specified)
+                   flags = 2)
 
-#     keypoint_trees = []
-#     for img_name in img_list:
-
-#         # extract sift_keypoints
-#         img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
-#         sift = cv2.xfeatures2d.SIFT_create()
-#         kp = sift.detect(img, None)
-
-#         # sort?
-#         # kp.sort( key = lambda k: k.response)
-#         # kp = kp[::-1]
-
-#         # create a KD-tree with the keypoints
-#         keypoint_trees.append(spatial.KDTree(kp))
-
-#     return keypoint_trees
-
-
-# # ---------------------------------------------------------------------------
-# def match_keypoints():
-
+    out_img = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, **draw_params)
+    cv2.imwrite(out_name, out_img)
 
 
 # -----------------------------------------------------------------------------
-def detect_and_match(img_list):
-    # find the keypoints and descriptors of the enchor with SIFT
-    img1 = cv2.imread(img_list[0], cv2.IMREAD_GRAYSCALE)
-    # sift = cv2.SIFT()
-    sift = cv2.xfeatures2d.SURF_create()
-    kp1, des1 = sift.detectAndCompute(img1, None)
+def detect_and_match(img_name1, img_name2):
 
-    for img_name in img_list[1:]:
+    _, filename = ntpath.split(img_name1)
+    out_name1, ext = filename.split(".")
+    _, filename = ntpath.split(img_name2)
+    out_name2, _ = filename.split(".")
 
-        # read SIFT keypoints and descriptors for the new image
-        img2 = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
-        # sift = cv2.SIFT()
-        kp2, des2 = sift.detectAndCompute(img2, None)
+    img1 = cv2.imread(img_name1, cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread(img_name2, cv2.IMREAD_GRAYSCALE)
+    
+    # detect and compute keypoints and descriptors for each image
+    # detector = cv2.xfeatures2d.SURF_create()
+    detector = cv2.xfeatures2d.SIFT_create()
+    kp1, des1 = detector.detectAndCompute(img1, None)
+    kp2, des2 = detector.detectAndCompute(img2, None)
 
-        # 'Fast Library for Approximate Nearest Neighbor search'
-        # Searh for approximate nearest neighbors between the anchor and
-        # the current image
-        FLANN_INDEX_KDTREE = 0
-        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-        search_params = dict(checks = 50)
-        flann = cv2.FlannBasedMatcher(index_params, search_params)
-        matches = flann.knnMatch(des1,des2,k=2)
+    # 'Fast Library for Approximate Nearest Neighbor search'
+    # Searh for approximate nearest neighbors between the two images
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1,des2,k=2)
 
-        # store all the good matches as per Lowe's ratio test
-        good = []
-        for m,n in matches:
-            if m.distance < 0.7*n.distance:
-                good.append(m)
+    # store all the good matches per Lowe's ratio test
+    good = []
+    for m,n in matches:
+        if m.distance < 0.7*n.distance:
+            good.append(m)
 
+    # draw all keypoints that pass the ratio test
+    out_name = out_name1 + "_" + out_name2 + "_before_F_mat." + ext
+    draw_keypoint_matches(img1, kp1, img2, kp2, good, out_name)
 
-
-    # check if the images match
+    # if the minimum threshold is met
     if len(good) > MIN_MATCH_COUNT:
-
         src_pts = np.float32([ kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
         dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
-
-        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+    
+        # calculate fundamental matrix, output surviving keypoint match
+        F, mask = cv2.findFundamentalMat(src_pts, dst_pts, cv2.FM_RANSAC, FM_RANSAC_THRESHOLD)
         matchesMask = mask.ravel().tolist()
+        print("Original: ", len(matchesMask))
+        print("Survived: ", np.sum(matchesMask))
+        out_name = out_name1 + "_" + out_name2 + "_after_F_mat." + ext
+        draw_keypoint_matches(img1, kp1, img2, kp2, good, out_name, matchesMask)
 
-        h,w = img1.shape
-        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-        dst = cv2.perspectiveTransform(pts,M)
+        # h,w = img1.shape
+        # pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        # dst = cv2.perspectiveTransform(pts, F)
 
-        img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+        # calculate homography
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        matchesMask = mask.ravel().tolist()
+        # dst = cv2.perspectiveTransform(pts,M)
+        # img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+
 
     else:
         print("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
         matchesMask = None
-
-    # draw matches on the images together
-    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   matchesMask = matchesMask, # draw only inliers
-                   flags = 2)
-
-    img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
-    cv2.imwrite("Keypoint Match.jpg", img3)
-
-    # plt.imshow(img3, 'gray'),plt.show()
 
 
 # =============================================================================
 if __name__ == "__main__":
 
     img_list = arg_parse()
-    MIN_MATCH_COUNT = 10
+    img_list.sort()
     print(img_list)
 
-    # extract a list containing a kd-tree of keypoints from each image
-    matches = detect_and_match(img_list)
+    # attempt to match adjacent images in the directory
+    for i in range(len(img_list[:-1])):
+        detect_and_match(img_list[i], img_list[i+1])
 
 
 
