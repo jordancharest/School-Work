3e2c4b9f32b63c24afc3a6169d8d15ecc508083a
Jordan Charest <jordancharest3@gmail.com>
2018-11-15 23:15:33 -0500

    Use glob to pull image names for each category

--- Computer-Vision/scene-classification/feature_extractor.py
+++ Computer-Vision/scene-classification/feature_extractor.py
@@ -1,6 +1,12 @@
 from sys import argv
+import glob
 
 import numpy
+
+# tunable parameters
+t = 4
+bh = 4  # number of blocks in image height direction
+bw = 4  # number of blocks in image width direction
 
 def arg_parse():
     if len(argv) == 2:
@@ -9,7 +15,7 @@ def arg_parse():
 
     else:
         print("Invalid Argument(s).")
-        print("USAGE: {} <training-data-directory>")
+        print("USAGE: {} <training-data-directory>".format(argv[0]))
         exit()
 
 
@@ -17,4 +23,13 @@ def arg_parse():
 if __name__ == "__main__":
     training_data_dir = arg_parse()
 
-    print(training_data_dir)
+    grass = glob.glob(training_data_dir + "/grass/*.JPEG")
+    ocean = glob.glob(training_data_dir + "/ocean/*.JPEG")
+    redcarpet = glob.glob(training_data_dir + "/redcarpet/*.JPEG")
+    road = glob.glob(training_data_dir + "/road/*.JPEG")
+    wheatfield = glob.glob(training_data_dir + "/wheatfield/*.JPEG")
+    print("Grass:", len(grass))
+    print("Ocean:", len(ocean))
+    print("Red carpet:", len(redcarpet))
+    print("Road:", len(road))
+    print("Wheatfield:", len(wheatfield))
