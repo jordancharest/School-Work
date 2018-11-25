d297fd2549ebafd861fb7dca5bc6628a02b36a20
Jordan Charest <jordancharest3@gmail.com>
2018-11-24 22:48:04 -0500

    Added second hidden layer

--- Computer-Vision/scene-classification/fully_connected.py
+++ Computer-Vision/scene-classification/fully_connected.py
@@ -5,12 +5,12 @@ class FullyConnected(nn.Module):
     def __init__(self, n, m):
         super(FullyConnected, self).__init__()
         self.fc1 = nn.Linear(n * m * 3, 200)
-        # self.fc2 = nn.Linear(200, 200)
+        self.fc2 = nn.Linear(200, 200)
         self.fc3 = nn.Linear(200, 5)
 
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
+        x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
