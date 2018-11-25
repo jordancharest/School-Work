d71bb8f32ca76d81e6df182b64504b9689cc7094
Jordan Charest <jordancharest3@gmail.com>
2018-11-24 20:32:35 -0500

    Switched to cross-entropy loss; lots of small changes

--- Computer-Vision/scene-classification/simple_NN_predictor.py
+++ Computer-Vision/scene-classification/simple_NN_predictor.py
@@ -2,6 +2,8 @@ from sys import argv
 import glob
 import time
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelBinarizer
 from torch import optim
 from torch.autograd import Variable
@@ -45,14 +47,14 @@ def preprocess(img_list, m=0, n=0):
     X = []
     y = []
     for img_name in img_list:
-        img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
+        img = cv2.imread(img_name, cv2.IMREAD_COLOR)
 
         # resize if applicable
         if m > 0 and n > 0:
             img = cv2.resize(img, (m, n))
         
         # normalize
-        img = (img - 128) / 128
+        # img = (img - 128) / 128
 
         # flatten and add to the dataset
         img = np.ravel(img)
@@ -70,125 +72,173 @@ def make_label_vectors(length):
     road = 3 * np.ones(length)
     wheatfield = 4 * np.ones(length)
 
-    labels = np.hstack((grass, ocean, redcarpet, road, wheatfield))
-    print(labels.shape)
+    y = np.hstack((grass, ocean, redcarpet, road, wheatfield))
 
     # Binarize the labels
-    lb = LabelBinarizer()
-    y = lb.fit_transform(labels)
-    print(y.shape)
+    # lb = LabelBinarizer()
+    # y = lb.fit_transform(y)
 
     return y
+# -----------------------------------------------------------------------------
+def get_data(directory, m, n, split=False, scaler=None):
+    img_names = get_img_names(directory)
+
+    # read and preprocess all images
+    start = time.time()
+    X = []
+    for j, img_set in enumerate(img_names):
+        X_partial, y_partial = preprocess(img_set, m, n)
+        X.append(X_partial)
+
+    # format, shape, and scale
+    X = np.array(X)
+    X = X.reshape(X.shape[0] * X.shape[1], X.shape[2])
+
+    if scaler == None:
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X).astype(np.uint8)
+    else:
+        X = scaler.transform(X).astype(np.uint8)
+
+    # build label vector
+    y = make_label_vectors(len(img_names[0]))
+
+
+    if split:
+        X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+    duration = round(time.time()-start, 2)
+    print(duration, "seconds to extract images and build label tensor")
+    print("Total images:", X.shape[0])
+    print("Flattened image size:", X.shape[1])
+
+    if split:
+        X_train = Variable(torch.tensor(X_train, dtype=torch.float))
+        y_train = Variable(torch.tensor(y_train, dtype=torch.long))
+        X_valid = Variable(torch.tensor(X_valid, dtype=torch.float))
+        y_valid = Variable(torch.tensor(y_valid, dtype=torch.long))
+        return X_train, y_train, X_valid, y_valid, scaler
+    else:
+        X = Variable(torch.tensor(X, dtype=torch.float))
+        y = Variable(torch.tensor(y, dtype=torch.long))
+        return X, y
 
 # -----------------------------------------------------------------------------
-def train(X_train, y_train, m, n, learning_rate=1e-4, momentum=0.9):
+def train(X_train, y_train, X_valid, y_valid, m, n, learning_rate=1e-5):
     #  Set parameters to control the process
-    epochs = 1000
+    num_epochs = 100
     batch_size = 16
-    print(X_train.size())
     n_train = X_train.size()[0]
     n_batches = int(np.ceil(n_train / batch_size))
     log_interval = 10
 
-    print("# batches:", n_batches)
-    print("# train:", n_train)
+    print("\nNumber of batches:", n_batches)
 
-    model = FullyConnected(24,36)
+    model = FullyConnected(m,n)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    criterion = nn.MSELoss()
+    criterion = nn.CrossEntropyLoss()
 
-    for epoch in range(epochs):
-        #  Create a random permutation of the indices of the row vectors
+    print("\n\nBegin Training...")
+    start = time.time()
+
+    for ep in range(num_epochs):
+        #  Create a random permutation of the indices of the row vectors.
         indices = torch.randperm(n_train)
         
         #  Run through each mini-batch
-        for batch_idx, b in enumerate(range(n_batches)):
-
-            # use the last batch of each epoch as validation set
-            if batch_idx == n_batches-1:
-                test(model, criterion, X_train[batch_indices], y_train[batch_indices])
-                continue
-
+        for b in range(n_batches):
             #  Use slicing (of the pytorch Variable) to extract the
             #  indices and then the data instances for the next mini-batch
-            batch_indices = indices[b*batch_size: (b+1)*batch_size]
+            batch_indices = indices[b*batch_size:(b+1)*batch_size]
             batch_X = X_train[batch_indices]
-            batch_y = y_train[batch_indices]
+            batch_Y = y_train[batch_indices]
             
-            # zero gradients, make prediction on the batch
-            optimizer.zero_grad()
+            #  Run the network on each data instance in the minibatch
+            #  and then compute the object function value
             y_pred = model(batch_X)
-
-            # compute loss and back-propagate
-            loss = criterion(y_pred, batch_y)
+            loss = criterion(y_pred, batch_Y)
+            
+            #  Back-propagate the gradient through the network using the
+            #  implicitly defined backward function, but zero out the
+            #  gradient first.  The step is made here by the optimizer
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        if epoch % log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, (batch_idx+1) * batch_size, n_train,
-                           100. * batch_idx / n_batches, loss.data[0]))
+        if ep != 0 and ep % log_interval == 0:
+            y_pred = model(X_valid)
+            valid_loss = criterion(y_pred, y_valid)
+            accuracy, correct, incorrect = success_rate(y_pred, y_valid)
+            print("Epoch %d:\nloss: %.5f, accuracy: %.2f" %(ep, valid_loss.item(), accuracy))
 
-    return model, optimizer, criterion
+    print(round(time.time()-start, 2), "seconds to train\n")
+    return model, criterion
 
 
 # -----------------------------------------------------------------------------
 def test(model, criterion, X_test, y_test):
-    # run a test loop
-    test_loss = 0
-    correct = 0
+    # make predictions
+    y_pred = model(X_test)
+    print("Y_pred:", y_pred.size())
+    print("Y_test:", y_test.size())
 
-    for X, y in zip(X_test, y_test):
-        y_pred = model(X)
+    # sum up batch loss and compute accuracy
+    # y_test = torch.Tensor.long(y_test)
+    loss = criterion(y_pred, y_test)
+    accuracy, correct, incorrect = success_rate(y_pred, y_test)
 
-        # sum up batch loss
-        test_loss += criterion(y_pred, y).data.item()
-        pred = y_pred.data.max(0)[1]  # get the index of the max in the prediction tensor
-
-        # check that the max of the predicted is the same as the label
-        correct += pred.eq(y.data.max(0)[1]).sum()
-
-    test_loss /= X_test.size()[0]
     print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
-            test_loss, correct, X_test.size()[0],
-            100. * correct / X_test.size()[0]))
+            loss, correct, X_test.size()[0], accuracy))
 
+# -----------------------------------------------------------------------------
+def success_rate(pred_Y, Y):
+    '''
+    Calculate and return the success rate from the predicted output Y and the
+    expected output.  There are several issues to deal with.  First, the pred_Y
+    is non-binary, so the classification decision requires finding which column
+    index in each row of the prediction has the maximum value.  This is achieved
+    by using the torch.max() method, which returns both the maximum value and the
+    index of the maximum value; we want the latter.  We do this along the column,
+    which we indicate with the parameter 1.  Second, the once we have a 1-d vector
+    giving the index of the maximum for each of the predicted and target, we just
+    need to compare and count to get the number that are different.  We could do
+    using the Variable objects themselve, but it is easier syntactically to do this
+    using the .data Tensors for obscure PyTorch reasons.
+    '''
+    print(pred_Y, pred_Y.size())
+    _,pred_Y_index = torch.max(pred_Y, 1)
+    _,Y_index = torch.max(Y,1)
+    num_equal = torch.sum(pred_Y_index.data == Y_index.data).item()
+    num_different = torch.sum(pred_Y_index.data != Y_index.data).item()
+    rate = num_equal / float(num_equal + num_different)
+    return rate, num_equal, num_different # rate.item()
 
 # =============================================================================
 if __name__ == "__main__":
     training_data, test_data = arg_parse()
 
+    # Desired image size
     m = 24
     n = 36
 
-    # data extraction
-    names = ["grass", "ocean", "redcarpet", "road", "wheatfield"]
-    training_img_names = get_img_names(training_data)
+    print("\nGetting training data")
+    X_train, y_train, X_valid, y_valid, scaler = get_data(training_data, m , n, split=True, scaler=None)
 
-    X_train = []
-    y_train = []
-    y_train = make_label_vectors(len(training_img_names[0]))
-    print(y_train)
-    for j, img_set in enumerate(training_img_names):
-        start = time.time()
-        X, y = preprocess(img_set, m, n)
-        print(round(time.time()-start, 2), "seconds to extract", names[j], "train features...")
-        print(X.shape)
-        X_train.append(X)
 
-    X_train = np.array(X_train)
-    X_train = X_train.reshape(X_train.shape[0] * X_train.shape[1], X_train.shape[2]) 
+    model, criterion = train(X_train, y_train, X_valid, y_valid, m, n)
 
-    X_train = Variable(torch.Tensor(X_train))
-    y_train = Variable(torch.Tensor(y_train)) 
-    model, criterion = train(X_train, y_train, m, n)
+    print("Getting test data")
+    X_test, y_test = get_data(test_data, m, n, split=False, scaler=scaler)
+    test(model, criterion, X_test, y_test)
+
+
 
 
 
 
 
     # extract the test images and labels
-    test(model, criterion, X_test, y_test)
+    test(model, criterion, X_test, y_test )
 
     
 
