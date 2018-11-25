bc2bcb43c7065cbb705dc69022734ce3b5967c9e
Jordan Charest <jordancharest3@gmail.com>
2018-11-24 20:32:04 -0500

    Removed a layer

--- Computer-Vision/scene-classification/fully_connected.py
+++ Computer-Vision/scene-classification/fully_connected.py
@@ -4,13 +4,13 @@ import torch.nn.functional as F
 class FullyConnected(nn.Module):
     def __init__(self, n, m):
         super(FullyConnected, self).__init__()
-        self.fc1 = nn.Linear(n * m, 200)
-        self.fc2 = nn.Linear(200, 200)
+        self.fc1 = nn.Linear(n * m * 3, 200)
+        # self.fc2 = nn.Linear(200, 200)
         self.fc3 = nn.Linear(200, 5)
 
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        # x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
