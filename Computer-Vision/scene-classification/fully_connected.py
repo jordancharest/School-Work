596fef9da445f778ef3315fce4caab532c28d3d6
Jordan Charest <jordancharest3@gmail.com>
2018-11-27 21:01:39 -0500

    Added another layer; sample output

--- Computer-Vision/scene-classification/fully_connected.py
+++ Computer-Vision/scene-classification/fully_connected.py
@@ -6,11 +6,13 @@ class FullyConnected(nn.Module):
         super(FullyConnected, self).__init__()
         self.fc1 = nn.Linear(n * m * 3, 200)
         self.fc2 = nn.Linear(200, 200)
-        self.fc3 = nn.Linear(200, 5)
+        self.fc3 = nn.Linear(200, 100)
+        self.fc4 = nn.Linear(100, 5)
 
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         return x
