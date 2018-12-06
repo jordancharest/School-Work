03f70292385c856d06ccd212a6ac71ffc8cacbee
Jordan Charest <jordancharest3@gmail.com>
2018-12-05 21:36:30 -0500

    Painted optical flow lines, but need to do it on color image

--- Computer-Vision/optical-flow/motion_detection.py
+++ Computer-Vision/optical-flow/motion_detection.py
@@ -1,14 +1,19 @@
 from sys import argv
 
 import cv2
+import numpy as np
+
+# TODO:
+# Keep a color version to draw on but compute optical flow on grayscale
+
 
 # -----------------------------------------------------------------------------
 def arg_parse():
     if len(argv) == 3:
-        script, img_name1, img_name2 = argv
+        script, img_name0, img_name1 = argv
+        img0 = cv2.imread(img_name0, cv2.IMREAD_GRAYSCALE)
         img1 = cv2.imread(img_name1, cv2.IMREAD_GRAYSCALE)
-        img2 = cv2.imread(img_name2, cv2.IMREAD_GRAYSCALE)
-        return img1, img2
+        return img0, img1
 
     else:
         print("Invalid Argument(s).")
@@ -16,11 +21,40 @@ def arg_parse():
         exit()
 
 # -----------------------------------------------------------------------------
-def optical_flow(img1, img2):
-    points1 = cv2.goodFeaturesToTrack(img1, mask=None, **feature_params)
+def optical_flow(img0, img1, feature_params, lk_params):
+    # Create some random colors
+    colors = np.random.randint(0,255,(100,3))
+    points0 = cv2.goodFeaturesToTrack(img0, mask=None, **feature_params)
+    points1, status, err = cv2.calcOpticalFlowPyrLK(img0, img1, points0, None, **lk_params)
+
+    # Create a mask image for drawing purposes
+    mask = np.zeros_like(img1)
+
+
+    # print("Points:\n", points1)
+    # print("Status:\n", status)
+    # print("Error:\n", err)
+
+    # Select good points
+    good0 = points0[status == 1]
+    good1 = points1[status == 1]
+
+    # draw the tracks
+    for i,(new,old) in enumerate(zip(good1,good0)):
+        a,b = new.ravel()
+        c,d = old.ravel()
+        mask = cv2.line(mask, (a,b), (c,d), colors[i].tolist(), 2)
+        img1 = cv2.circle(img1, (a,b), 5, colors[i].tolist(), -1)
+    result = img1 + mask
+    cv2.imwrite("result.png", result)
+
+    # print()
+
 
 # =============================================================================
 if __name__ == "__main__":
+    img0, img1 = arg_parse()
+
     # params for ShiTomasi corner detection
     feature_params = dict( maxCorners = 100,
                            qualityLevel = 0.3,
@@ -29,7 +63,6 @@ if __name__ == "__main__":
     # Parameters for lucas kanade optical flow
     lk_params = dict( winSize  = (15,15),
                       maxLevel = 2,
-                      criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
+                      criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-    img1, img2 = arg_parse()
-    optical_flow(img1, img2, feature_params, lk_params)
+    optical_flow(img0, img1, feature_params, lk_params)
