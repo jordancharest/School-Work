2598569985a4ccd75cc1c70eee8dc802877571d2
Jordan Charest <jordancharest3@gmail.com>
2018-12-09 16:24:29 -0500

    Focus of expansion calculated using RANSAC

--- Computer-Vision/optical-flow/motion_detection.py
+++ Computer-Vision/optical-flow/motion_detection.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 
 point_motion_threshold = 5
-camera_motion_threshold = 0.9
+camera_motion_threshold = 0.8
 
 
 # -----------------------------------------------------------------------------
@@ -61,12 +61,80 @@ def optical_flow(img0, img1, feature_params, lk_params):
         return False, good0, good1
 
 # -----------------------------------------------------------------------------
-def estimate_focus_of_expansion(points0, points1):
+def FoE_RANSAC(pts0, pts1, samples=50, tau=0.75):
     print("RANSAC'n")
+    k_max = 0
+    best_i, best_j = 0,1
+    best_intersection = None
+
+    # RANSAC - pick two random lines, find their intersection, then compute the
+    # distance between that intersection and all other lines
+    for num in range(samples):
+        i,j = np.random.randint(0, len(pts0), 2)
+
+        # don't attempt to find an intersection with itself
+        if i == j:
+            continue
+
+        # find the intersection of two optical flow lines
+        intersection = find_intersection(pts0[i], pts1[i], pts0[j], pts1[j])
+
+        # calculate the distance between every other optical flow line and this intersection
+        error = np.cross(pts1-pts0, pts0-intersection) / np.linalg.norm(pts1-pts0)
+        error = error**2
+
+        # only keep the points that are less than the tolerance
+        error = error[error < tau**2]
+        k = len(error)
+
+        # if we have a new best fit, then save it
+        if k > k_max:
+            best_i, best_j = i,j
+            best_intersection = intersection
+            k_max = k
+
+            # output statistics about this fit
+            print("Sample {0}:".format(num))
+            print("indices ({0},{1})".format(i, j))
+            print("inliers", k)
+            print()
+
+    # calculate inlier and outlier average distances
+    distances = np.cross(pts1-pts0, pts0-intersection) / np.linalg.norm(pts1-pts0)
+    error = distances**2
+    inliers = distances[error < tau**2]
+    outliers = distances[error >= tau**2]
+
+    print("avg inlier dist {0:.3f}".format(np.average(inliers)))
+    print("avg outlier dist {0:.3f}".format(np.average(outliers)))
+
+    return int(best_intersection[0]), int(best_intersection[1])
+
+
+# -----------------------------------------------------------------------------
+# From here: https://stackoverflow.com/a/42727584
+def find_intersection(a1, a2, b1, b2):
+    """ 
+    Returns the point of intersection of the lines passing through a2,a1 and b2,b1.
+    a1: [x, y] a point on the first line
+    a2: [x, y] another point on the first line
+    b1: [x, y] a point on the second line
+    b2: [x, y] another point on the second line
+    """
+    s = np.vstack([a1,a2,b1,b2])        # s for stacked
+    h = np.hstack((s, np.ones((4, 1)))) # h for homogeneous
+    l1 = np.cross(h[0], h[1])           # get first line
+    l2 = np.cross(h[2], h[3])           # get second line
+    x, y, z = np.cross(l1, l2)          # point of intersection
+    if z == 0:                          # lines are parallel
+        return (float('inf'), float('inf'))
+    return (x/z, y/z)
+
 
 # =============================================================================
 if __name__ == "__main__":
     img0, img1 = arg_parse()
+    foe_img = np.copy(img1)
 
     # params for ShiTomasi corner detection
     feature_params = dict( maxCorners = 100,
@@ -78,6 +146,12 @@ if __name__ == "__main__":
                       maxLevel = 2,
                       criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-    camera_is_moving, points0, points1 = optical_flow(img0, img1, feature_params, lk_params)
+    # determine if camera is moving
+    camera_is_moving, pts0, pts1 = optical_flow(img0, img1, feature_params, lk_params)
+    
+    # if it is, estimate the focus of expansion
     if camera_is_moving:
-        estimate_focus_of_expansion(points0, points1)
+        FoE = FoE_RANSAC(pts0, pts1)
+        foe_img = cv2.circle(foe_img, FoE, radius=10, color=(0,0,255), thickness=-1)
+        foe_img = cv2.circle(foe_img, FoE, radius=5, color=(255,255,255), thickness=2)
+        cv2.imwrite("focus_of_expansion.png", foe_img)
